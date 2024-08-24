3b7ab85d3584500cddffe01dc51c5223f066a9eb
tomsains <thomas.sainsbury@epfl.ch>
2024-08-24 14:27:11 +0200

    added bot based on sentiment

--- buy_and_hold.py
+++ buy_and_hold.py
@@ -3,12 +3,16 @@ from lumibot.strategies.strategy import Strategy
 from lumibot.traders import Trader
 from datetime import datetime 
 from alpaca_trade_api import REST 
+from lumibot.brokers import Alpaca
+from Alpaca_creds import ALPACA_CREDS_PAPER
+from timedelta import Timedelta 
+
 
 
 class BuyAndHold(Strategy):
-    def initialize(self, stock = "AAPL"):
+    def initialize(self, stock = "PLTR"):
         self.stock = stock
-        self.sleeptime("24H")
+        self.sleeptime ="24H"
     
     def on_trading_iteration(self):
        
@@ -26,12 +30,14 @@ class BuyAndHold(Strategy):
 
 # Pick the dates that you want to start and end your backtest
 backtesting_start = datetime(2023, 11, 1)
-backtesting_end = datetime.now()
+backtesting_end = datetime.now() - Timedelta(days=1)
 
 
 if __name__ == '__main__':
     # Run the backtest
-    strat = BuyAndHold(name = "buy_and_hold", broker=None, parameters={})
+    #print(ALPACA_CREDS_PAPER)
+    broker = Alpaca(ALPACA_CREDS_PAPER)
+    strat = BuyAndHold(name = "buy_and_hold", broker=broker, parameters={})
     strat.backtest(
         YahooDataBacktesting,
         backtesting_start,
